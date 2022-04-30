44d4c98ab0ce189a39f5064d286efdf3773e2635
Meir Sedlis <meirsedlis@gmail.com>
2022-04-30 15:58:39 -0500

    moved the like button outside of the image div

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,33 +1,51 @@
 import { useState } from "react";
 
 function GalleryItem({ galleryItem }) {
+  const [showPhoto, setShowPhoto] = useState(true);
 
-    const [showPhoto, setShowPhoto] = useState(true);
+  const flipPhoto = () => {
+    setShowPhoto(!showPhoto);
+  };
+  const renderGalleryItem = () => {
+    if (showPhoto) {
+      return <img src={galleryItem.path} />;
+    } else {
+      return (
+        <div class="galleryItem">
+          <br />
+          <p>Description:</p>
+          {galleryItem.description}
+        </div>
+      );
+    }
+  };
 
-    const flipPhoto = () =>{
-        setShowPhoto(!showPhoto)
+  const renderLikes = () => {
+    if (galleryItem.likes) {
+      return (
+        <div>
+          <p>{galleryItem.likes} people like this photo!</p>
+          <button>👍 Like</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <p>No one likes this yet!</p>
+          <button>👍 Like</button>
+        </div>
+      );
     }
-    const renderGalleryItem = () => {
-        if(showPhoto){
-            return (<img src={galleryItem.path} />)
-        } else{
-            return ( <div class='galleryItem'>
-            <br />
-             <p>Description:</p>
-           {galleryItem.description}
-           <p>Likes:</p>
-           {galleryItem.likes} <br /> <br />
-           <button>👍 Like</button>
-         </div>)
-        }
-    }
+  };
+
   const photo = <img src={galleryItem.path} />;
 
   console.log(galleryItem);
   return (
-   <div  onClick={flipPhoto}>
-    {renderGalleryItem()}
-   </div>
+    <div onClick={flipPhoto}>
+      {renderGalleryItem()}
+      {renderLikes()}
+    </div>
   );
 }
 
