f798f3dd7844d5f40bd64f14fe60b9d1edfbd287
Meir Sedlis <meirsedlis@gmail.com>
2022-04-30 13:55:20 -0500

    updated gallery.data to include more reaction animals

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/generousBird.jpg', description: 'Photo of a quaker parrot offering a pen.', likes: 15 },
-    { id: 2, path: 'images/wondrousBird.jpg', description: 'Photo of a bird expressing wonder.', likes: 3 }
+    { id: 1, path: 'images/generousBird.jpg', description: 'Photo of a quaker parrot offering a pen.', likes: 0 },
+    { id: 2, path: 'images/wondrousBird.jpg', description: 'Photo of a bird expressing wonder.', likes: 0 },
+    { id: 3, path: 'images/knowingCow.jpg', description: 'Photo of a cow behind a wire fence', likes: 0 },
+    { id: 4, path: 'images/furiousBird.jpg', description: 'Photo of an angry bird', likes: 0 },
+    { id: 5, path: 'images/vengefulTurtle.jpg', description: 'Photo of a turtle with with murder in its heart', likes: 0 },
+    { id: 6, path: 'images/relievedBird.jpg', description: 'Photo of a relieved bird', likes: 0 },
+    { id: 7, path: 'images/yellingTurtle.jpg', description: 'Photo of an incredulous turtle', likes: 0 },
+    { id: 8, path: 'images/wowBirds.jpg', description: 'Photo of alarmed birds', likes: 0 },
 ];
 
 module.exports = galleryItems;
