302117946b663ff6801b8db5a31598f24e01c702
Meir Sedlis <meirsedlis@gmail.com>
2022-04-30 15:42:04 -0500

    added conditional formatting to GallertItem

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,34 @@
-function GalleryItem({galleryItem}){
-    console.log(galleryItem)
-    return (
-        <img src={galleryItem.path}/>
-    )
+import { useState } from "react";
+
+function GalleryItem({ galleryItem }) {
+
+    const [showPhoto, setShowPhoto] = useState(true);
+
+    const flipPhoto = () =>{
+        setShowPhoto(!showPhoto)
+    }
+    const renderGalleryItem = () => {
+        if(showPhoto){
+            return (<img src={galleryItem.path} />)
+        } else{
+            return ( <div class='galleryItem'>
+            <br />
+             <p>Description:</p>
+           {galleryItem.description}
+           <p>Likes:</p>
+           {galleryItem.likes} <br /> <br />
+           <button>👍 Like</button>
+         </div>)
+        }
+    }
+  const photo = <img src={galleryItem.path} />;
+
+  console.log(galleryItem);
+  return (
+   <div  onClick={flipPhoto}>
+    {renderGalleryItem()}
+   </div>
+  );
 }
 
 export default GalleryItem;
